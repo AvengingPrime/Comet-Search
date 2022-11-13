ea7c99ac8ed0b65aaba7436023749ba3cdf7045d
AvengingPrime <vivanshah19@hotmail.com>
2022-11-13 12:50:51 -0600

    actually added guide card

---
+++ components/GuideCard.jsx
@@ -0,0 +1,21 @@
+import {React} from 'react'
+import {Container, Header, Icon, Button, Segment, Image} from 'semantic-ui-react'
+
+export default function GuideCard(props)
+{
+    const articles = props.articles
+
+    return (
+        <Segment.Group size = {'huge'}>
+            {articles.map((article) => (
+                <Segment key = {article}>
+                    <Header href = 'https://dev.guide.utdnebula.com/'>
+                        {article.title}
+                    </Header>
+                    {/* <Container> */}
+                    {article.text}<br/><br/>
+                    {/* </Container> */}
+                </Segment>))}
+        </Segment.Group>
+    );
+}
