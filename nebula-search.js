c5147d5893a86e3ebe3539f204fecec8ba60e295
Vivian Young <vivianbelleyoung@gmail.com>
2022-11-12 20:40:18 -0600

    added course

--- nebula-search.js
+++ nebula-search.js
@@ -19,40 +19,45 @@ async function main() {
 
   const keyword_extractor = require("keyword-extractor")
 
-  query = "is professor john Barden hard"
+  query = "is math 2417 hard"
 
   const query_extraction =
   keyword_extractor.extract(query,{
       language:"english",
-      remove_digits: true,
+      remove_digits: false,
       return_changed_case: true,
       remove_duplicates: true
   });
 
   console.log(query_extraction)
 
+  //Professor search
   professor_output = null
 
-  for (let i = 0; i < query_extraction.length-1; i++) {
-      first_keyword = query_extraction[i]
-      last_keyword = query_extraction[i+1]
+  //Check first and last name
+  if (query_extraction.length >= 2) {
+    for (let i = 0; i < query_extraction.length-1; i++) {
+        first_keyword = query_extraction[i]
+        last_keyword = query_extraction[i+1]
 
-      first_keyword = capitalizeFirstLetter(first_keyword)
-      last_keyword = capitalizeFirstLetter(last_keyword)
+        first_keyword = capitalizeFirstLetter(first_keyword)
+        last_keyword = capitalizeFirstLetter(last_keyword)
 
-      professor_params = { first_name: first_keyword, last_name: last_keyword};
-      professor_search_params = new URLSearchParams(professor_params);
-      professor_url = 'https://api.utdnebula.com/professor/?' + professor_search_params.toString()
+        professor_params = { first_name: first_keyword, last_name: last_keyword};
+        professor_search_params = new URLSearchParams(professor_params);
+        professor_url = 'https://api.utdnebula.com/professor/?' + professor_search_params.toString()
 
-      console.log(professor_url)
-      professor_call_nebula = await call_nebula(professor_url)
+        console.log(professor_url)
+        professor_call_nebula = await call_nebula(professor_url)
 
-      if (professor_call_nebula.data != null){
-        professor_output = professor_call_nebula.data
-        break
+        if (professor_call_nebula.data != null){
+          professor_output = professor_call_nebula.data
+          break
+        }
       }
     }
 
+  //Check last name
   if (professor_output = null){
     for (let i = 0; i < query_extraction.length; i++) {
       first_keyword = query_extraction[i]
@@ -75,8 +80,32 @@ async function main() {
 
   console.log(professor_output)
 
+  //Course prefix + number search
+  course_output = null
+
+  for (let i = 0; i < query_extraction.length-1; i++) {
+    first_keyword = query_extraction[i]
+    last_keyword = query_extraction[i+1]
+
+    first_keyword = first_keyword.toUpperCase()
+    last_keyword = last_keyword.toUpperCase()
+
+    course_params = { subject_prefix: first_keyword, course_number: last_keyword};
+    course_search_params = new URLSearchParams(course_params);
+    course_url = 'https://api.utdnebula.com/course/?' + course_search_params.toString()
+
+    console.log(course_url)
+    course_call_nebula = await call_nebula(course_url)
+
+    if (course_call_nebula.data != null){
+      course_output = course_call_nebula.data
+      break
+    }
+  }
+
+  console.log(course_output)
 }
 
 
+
 main()
-//console.log(professor_output)
